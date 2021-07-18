118a6a9ac126f27e24115a07f8937d4ec7a59632
aclark <adam.tclark@gmail.com>
2021-07-18 14:34:59 +0100

    removed loop

--- tmp/humidity/DHT11_multi.py
+++ tmp/humidity/DHT11_multi.py
@@ -12,20 +12,20 @@ instance_12 = dht11.DHT11(pin=12)
 instance_01 = dht11.DHT11(pin=1)
 
 try:
-  while True:
-      result_12 = instance_12.read()
-      result_01 = instance_01.read()
-      if result_12.is_valid():
-          print("Last valid input: " + str(datetime.datetime.now()))
+  #while True:
+  result_12 = instance_12.read()
+  result_01 = instance_01.read()
+  if result_12.is_valid():
+      print("Last valid input: " + str(datetime.datetime.now()))
 
-          print("Temperature 01: %-3.1f C" % result_12.temperature)
-          print("Humidity 01: %-3.1f %%" % result_12.humidity)
+      print("Temperature 01: %-3.1f C" % result_12.temperature)
+      print("Humidity 01: %-3.1f %%" % result_12.humidity)
 
-          if result_01.is_valid():
-              print("Temperature 02: %-3.1f C" % result_01.temperature)
-              print("Humidity 02: %-3.1f %%" % result_01.humidity)
+      if result_01.is_valid():
+          print("Temperature 02: %-3.1f C" % result_01.temperature)
+          print("Humidity 02: %-3.1f %%" % result_01.humidity)
 
-      time.sleep(10)
+  # time.sleep(10)
 
 except KeyboardInterrupt:
     print("Cleanup")
