5674c883c36d0aa6726aa99911ea01113a7de17b
Adam Clark <adam.tclark@gmail.com>
2021-07-19 09:41:40 +0200

    try to add original code for humidity

--- tmp/joint/joint_measure.py
+++ tmp/joint/joint_measure.py
@@ -13,33 +13,6 @@ for sensor in W1ThermSensor.get_available_sensors():
     print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
 sleep(1)
 
-# Humidity:
-# initialize GPIO
-GPIO.setwarnings(True)
-GPIO.setmode(GPIO.BCM)
-
-# read data using pin 12 and 1
-instance_12 = dht11.DHT11(pin=12)
-instance_01 = dht11.DHT11(pin=1)
-
-try:
-  result_12 = instance_12.read()
-  if result_12.is_valid():
-      print("Last valid input: " + str(datetime.datetime.now()))
-
-      print("Temperature 01: %-3.1f C" % result_12.temperature)
-      print("Humidity 01: %-3.1f %%" % result_12.humidity)
-  sleep(1)
-  result_01 = instance_01.read()
-  if result_01.is_valid():
-      print("Temperature 02: %-3.1f C" % result_01.temperature)
-      print("Humidity 02: %-3.1f %%" % result_01.humidity)
-
-except KeyboardInterrupt:
-    print("Cleanup")
-    GPIO.cleanup()
-sleep(1)
-
 # Moisture:
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
@@ -60,3 +33,34 @@ tmp = datetime.datetime.now()
 
 camera.capture('out/picture_'+tmp.strftime("%d.%m.%Y_%H.%M.%S")+'.jpg')
 camera.stop_preview()
+
+
+
+# Humidity
+# initialize GPIO
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BCM)
+
+# read data using pin 12 and 1
+instance_12 = dht11.DHT11(pin=12)
+instance_01 = dht11.DHT11(pin=1)
+
+try:
+  #while True:
+  result_12 = instance_12.read()
+  result_01 = instance_01.read()
+  if result_12.is_valid():
+      print("Last valid input: " + str(datetime.datetime.now()))
+
+      print("Temperature 01: %-3.1f C" % result_12.temperature)
+      print("Humidity 01: %-3.1f %%" % result_12.humidity)
+
+      if result_01.is_valid():
+          print("Temperature 02: %-3.1f C" % result_01.temperature)
+          print("Humidity 02: %-3.1f %%" % result_01.humidity)
+
+  # time.sleep(10)
+
+except KeyboardInterrupt:
+    print("Cleanup")
+    GPIO.cleanup()
