f3dfbd8e2dcd2200784963552ffda4f50040275a
aclark <adam.tclark@gmail.com>
2021-07-20 22:40:14 +0200

    updated to measure and upload every 60 seconds

--- src/loop_piecewise.sh
+++ src/loop_piecewise.sh
@@ -10,21 +10,24 @@ time_name="../data/time_data_${rpid}.txt"
 while true
 do
 counter=1
-while [ $counter -le 10 ]
+while [ $counter -le 30 ]
 do
     date
     python3 air_humtemp.py >> ${air_humtemp_name}
     python3 soil_moist.py >> ${soil_moist_name}
     python3 soil_temp.py >> ${soil_temp_name}
     python3 gettime.py >> ${time_name}
-    sleep 10
+    sleep 60
     ((counter++))
+    rpid=`cat ../../idinfo/sensorid`
+    git commit -m "auto upload from ${rpid}" -a
+    git push
 done
 
 python3 takepic.py
 
 rpid=`cat ../../idinfo/sensorid`
 git add ../data/photos/*
-git commit -m "auto upload from ${rpid}" -a
+git commit -m "auto photo upload from ${rpid}" -a
 git push
 done
