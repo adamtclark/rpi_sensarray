5618cae6778ac079c1c7135d76745444575efcb9
aclark <adam.tclark@gmail.com>
2021-07-20 22:51:27 +0200

    updated loop script

--- src/loop_piecewise.sh
+++ src/loop_piecewise.sh
@@ -6,28 +6,27 @@ air_humtemp_name="../data/air_humtemp_data_${rpid}.txt"
 soil_moist_name="../data/soil_moist_data_${rpid}.txt"
 soil_temp_name="../data/soil_temp_data_${rpid}.txt"
 time_name="../data/time_data_${rpid}.txt"
+rpid=`cat ../../idinfo/sensorid`
 
 while true
 do
 counter=1
 while [ $counter -le 30 ]
 do
-    date
     python3 air_humtemp.py >> ${air_humtemp_name}
     python3 soil_moist.py >> ${soil_moist_name}
     python3 soil_temp.py >> ${soil_temp_name}
     python3 gettime.py >> ${time_name}
-    sleep 60
+    date
+    git commit -m "auto upload from ${rpid}" -a || true
+    git push || true
+    sleep 30
     ((counter++))
-    rpid=`cat ../../idinfo/sensorid`
-    git commit -m "auto upload from ${rpid}" -a
-    git push
 done
 
 python3 takepic.py
-
-rpid=`cat ../../idinfo/sensorid`
-git add ../data/photos/*
-git commit -m "auto photo upload from ${rpid}" -a
-git push
+echo photo
+git add ../data/photos/* || true
+git commit -m "auto photo upload from ${rpid}" -a || true
+git push || true
 done
