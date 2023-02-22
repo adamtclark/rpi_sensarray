0d5ee204903fd2af57b874df54d05f5e52731aad
aclark <adam.tclark@gmail.com>
2023-02-22 13:48:50 +0100

    updated to run with DH22

--- src/air_humtemp.py
+++ src/air_humtemp.py
@@ -1,35 +1,40 @@
-import RPi.GPIO as GPIO
-import dht11
+#import RPi.GPIO as GPIO
+import Adafruit_DHT as dht
 import time
 import datetime
 tmp = datetime.datetime.now()
 
 # initialize GPIO
-GPIO.setwarnings(True)
-GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(True)
+#GPIO.setmode(GPIO.BCM)
 
 # read data using pin 12 and 1
-instance_12 = dht11.DHT11(pin=12)
-instance_01 = dht11.DHT11(pin=1)
+#instance_12 = dht.DHT22(pin=12)
+#instance_01 = dht.DHT22(pin=1)
 
-try:
-        trigger = True
-        while trigger:
-            result_12 = instance_12.read()
-            if result_12.is_valid():
-                trigger = False
-            else:
-                time.sleep(0.2)
+#try:
+#        trigger = True
+#        while trigger:
+#            result_12 = instance_12.read()
+#            if result_12.is_valid():
+#                trigger = False
+#            else:
+#                time.sleep(0.2)
+#
+#        trigger = True
+#        while trigger:
+#            result_01 = instance_01.read()
+#            if result_01.is_valid():
+#                trigger = False
+#            else:
+#                time.sleep(0.2)
+#        print(tmp.strftime("%Y.%m.%d_%H.%M.%S")+"; "+"AT01: "+str(result_12.temperature)+"; AH01: "+str(result_12.humidity)+"; AT02: "+str(result_01.temperature)+"; AH02: "+str(result_01.humidity))
+#
+#except KeyboardInterrupt:
+#        print("Cleanup")
+#        GPIO.cleanup()
 
-        trigger = True
-        while trigger:
-            result_01 = instance_01.read()
-            if result_01.is_valid():
-                trigger = False
-            else:
-                time.sleep(0.2)
-        print(tmp.strftime("%Y.%m.%d_%H.%M.%S")+"; "+"AT01: "+str(result_12.temperature)+"; AH01: "+str(result_12.humidity)+"; AT02: "+str(result_01.temperature)+"; AH02: "+str(result_01.humidity))
+h1,t1 = dht.read_retry(dht.DHT22, 1)
+h2,t2 = dht.read_retry(dht.DHT22, 12)
 
-except KeyboardInterrupt:
-        print("Cleanup")
-        GPIO.cleanup()
+print(tmp.strftime("%Y.%m.%d_%H.%M.%S")+"; "+"AT01: "+str(t1)+"; AH01: "+str(h1)+"; AT02: "+str(t2)+"; AH02: "+str(h2))
