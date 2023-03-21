2866e10927a355037e5df27bfc9c6c52bc97a9e8
aclark <adam.tclark@gmail.com>
2023-03-21 14:30:47 +0100

    updated to add in shutter closing and opening

--- src/takepic_RGB.py
+++ src/takepic_RGB.py
@@ -1,6 +1,25 @@
+import RPi.GPIO as GPIO
+
 from picamera import PiCamera
 from time import sleep
 import datetime
+
+
+GPIO.setmode(GPIO.BCM)
+
+#GPIO  5  als output
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+
+
+#GPIO 5 auf HIGH setzen
+GPIO.output(5, GPIO.HIGH)
+
+#GPIO 6 auf LOW
+GPIO.output(6, GPIO.LOW)
+
+sleep(2)
+
 
 camera = PiCamera()
 camera.start_preview()
@@ -11,3 +30,15 @@ with open('../../idinfo/sensorid') as f:
 
 camera.capture('../data/photos/picture_RGB_'+contents.strip()+"_"+tmp.strftime("%Y.%m.%d_%H.%M.%S")+'.jpg')
 camera.stop_preview()
+
+sleep(2)
+
+#change direction
+GPIO.output(5, GPIO.LOW)
+GPIO.output(6, GPIO.HIGH)
+
+sleep(2)
+
+#remove all GPIO settings
+GPIO.cleanup()
+
